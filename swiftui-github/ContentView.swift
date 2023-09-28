acb1daee6e5828b3dee46a77d2b211f50bd691ed
cikoedofebrian <ciko.edo.febrians@gmail.com>
2023-09-29 04:01:35 +0700

    Initial Commit

---
+++ swiftui-github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-github
+//
+//  Created by Ciko Edo Febrian on 29/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-github/swiftui_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_githubApp.swift
+//  swiftui-github
+//
+//  Created by Ciko Edo Febrian on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
